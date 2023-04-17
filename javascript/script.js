5f9ce1fcd0302eab8a221ad167eeab607724cdfe
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-17 11:30:43 -0300

    Subindo projeto até agora

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+function validar(){
+    let usuario = document.getElementById("usuario").value
+    let senha = document.getElementById("senha").value
+    
+    if(usuario == "" && senha == ""){
+        alert("Preencha todos os campos para prosseguir.")
+    }else{
+        alert("Acesso permitido.")
+        window.open("menu.html")
+    }
+}
