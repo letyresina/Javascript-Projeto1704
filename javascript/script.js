4ac50e587d6c72f9d62574d77ada965c9b985000
letyresina <leticiaresina@gmail.com>
2023-04-17 11:46:52 -0300

    Subindo o programa

--- javascript/script.js
+++ javascript/script.js
@@ -11,3 +11,27 @@ function validar(){
         window.open("menu.html")
     }
 }
+
+function calc(){
+    let n1 = Number(prompt("Digite o primeiro valor"))
+    let n2 = Number(prompt("Digite o segundo valor"))
+    let opcao = Number(prompt(`Dados Informados ${n1} e ${n2} \n Escolha uma opção: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão`))
+
+    switch(opcao){
+        case 1:
+            msg.innerHTML += `<p>${n1} + ${n2} = ${n1 + n2}</p>`
+            break;
+        case 2:
+            msg.innerHTML += `<p>${n1} - ${n2} = ${n1 - n2}</p>`
+            break;
+        case 3:
+            msg.innerHTML += `<p>${n1} * ${n2} = ${n1 * n2}</p>`
+            break;
+        case 4:
+            msg.innerHTML += `<p>${n1} / ${n2} = ${n1 / n2}</p>`
+            break; 
+        case 5:
+            msg.innerHTML += `Opção inválida`
+            break;
+    }
+}
